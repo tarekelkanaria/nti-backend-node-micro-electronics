011153aee69a6626f5fde40038e2e2d95637998b
tarekelkanaria <tarekelkanaria@gmail.com>
2026-02-14 11:55:17 +0200

    Phase 1 completed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
-express = require("express");
-const app = express();
+const express = require("express");
 const mongoose = require("mongoose");
-const port = process.env.PORT;
+const bcrypt = require("bcrypt");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const saltRounds = parseInt(process.env.SALTROUNDS) || 10;
 
 app.use(express.json());
 
@@ -18,8 +21,60 @@ async function connectDB() {
 
 connectDB();
 
+const User = require("./models/User");
+
 app.get("/", (req, res) => {
   res.send("Welcome to Micro Electronic home ");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    // get user data
+    const { username, email, password, role } = req.body;
+    // validate data
+    if (!username || !email || !password) {
+      return res.status(400).json({ msg: "Invalid Data" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(401).json({ msg: "Account Already Exist" });
+    }
+    if (password.length < 6)
+      return res.status(400).json({ msg: "Password is too small" });
+
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashPassword = await bcrypt.hash(password, salt);
+    // Create new user
+    const user = await User.create({
+      username,
+      email,
+      password: hashPassword,
+      role,
+    });
+    // Response
+    res.status(201).json({ msg: "Created Done!", data: user });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).json({ msg: "Missing Data" });
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ msg: "User Not Found" });
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch)
+      return res.status(401).json({ msg: "Invalid Password" });
+
+    res.status(200).json({ msg: "Login successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
